6e8f1c062e1b9cb0783a6680e0e5d3aba5a8b6d5
Guy Berkovich <mailofguy@gmail.com>
2021-03-04 15:35:15 +0200

    update footer style

--- xt-client/components/footer.js
+++ xt-client/components/footer.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
 import styles from '../styles/Footer.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
-const Footer = props => {
+const Footer = () => {
   return (
     <footer className={ styles.Footer }>
       <Container>
-        <Row className={ [styles.Row, styles.Centerized].join(' ') }>
+        <Row className={ styles.Centerized }>
           <Col xs="1">
-            <a href="https://www.linkedin.com/in/guy-berkovich-8a4892123/" target="_blank"><FontAwesomeIcon width="40px" fixedWidth icon={ faLinkedin } /></a>
+            <a href="https://www.linkedin.com/in/guy-berkovich-8a4892123/" target="_blank"><FontAwesomeIcon size="1x" icon={ faLinkedin } /></a>
           </Col>
           <Col xs="1">
-            <a href="https://github.com/GuyBerko" target="_blank"><FontAwesomeIcon width="40px" fixedWidth icon={ faGithubSquare } /></a>
+            <a href="https://github.com/GuyBerko" target="_blank"><FontAwesomeIcon size="1x" icon={ faGithubSquare } /></a>
           </Col>
         </Row>
-        <Row xs="1" className={ styles.Row }>
+        <Row xs="1">
           <Col sm="12" md={ { size: 6, offset: 3 } }>
             <span>&copy; 2020 - { new Date().getFullYear() } <a className={ styles.Link } href="mailto:mailofguy@gmail.com">Guy Berkovich</a>. All rights reserved.</span>
           </Col>
@@ -25,10 +24,6 @@ const Footer = props => {
       </Container>
     </footer>
   )
-}
-
-Footer.propTypes = {
-
 }
 
 export default Footer;
