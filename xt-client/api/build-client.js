da6b460a5bc6a98e6e6570c720c815d5092d5e43
Guy Berkovich <mailofguy@gmail.com>
2021-03-04 15:34:10 +0200

    add Credentials to requests

--- xt-client/api/build-client.js
+++ xt-client/api/build-client.js
@@ -4,7 +4,8 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
       baseURL: process.env.NODE_ENV === 'production' ? 'http://www.guy-berkovich.com' : 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers
+      headers: req.headers,
+      withCredentials: true
     });
   }
   return axios.create({ baseURL: '/' });
