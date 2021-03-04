35b3a6d72ff86dac87f57738175a374d2a19f91b
Guy Berkovich <mailofguy@gmail.com>
2021-03-04 15:40:08 +0200

    add validation

--- xt-client/pages/auth/[type].js
+++ xt-client/pages/auth/[type].js
@@ -1,42 +1,146 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
+import styles from '../../styles/Auth.module.scss';
+import FloatingLabelInput from '../../components/floating-label-input';
+import Validators from '../../utils/validators';
+import { Button, Form } from 'reactstrap';
+import params from '../../constants/auth.json';
 
 const Auth = ({ type }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [onSignin, errors] = useRequest({
+  const [name, setName] = useState('');
+  const [emailValidation, setEmailValidation] = useState(null);
+  const [passwordValidation, setPasswordValidation] = useState(null);
+  const [nameValidation, setNameValidation] = useState(null);
+  const [onAuth, errors] = useRequest({
     url: `/api/users/${type}`,
     method: 'post',
-    body: {
-      email,
-      password
-    },
+    body: {},
     onSuccess: () => Router.push('/')
   });
 
-  const submitText = type === 'signin' ? 'Sign In' : 'Sign Up';
+  if (!params[type]) {
+    throw new Error('Not Found');
+  }
+
+  const onChange = (e) => {
+    const value = e.target.value;
+    const type = e.target.name;
+
+    switch (type) {
+      case 'email': {
+        setEmail(value);
+        if (emailValidation !== null) {
+          setEmailValidation(Validators.email(value));
+        }
+        break;
+      }
+      case 'name': {
+        setName(value);
+        if (nameValidation !== null) {
+          setNameValidation(Validators.name(value));
+        }
+        break;
+      }
+      case 'password': {
+        setPassword(value);
+        if (passwordValidation !== null) {
+          setPasswordValidation(Validators.password(value));
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
+  const onBlur = (e) => {
+    const value = e.target.value;
+    const type = e.target.name;
+
+    validateField(type, value);
+  };
+
+  const validateField = (type, value) => {
+    switch (type) {
+      case 'email': {
+        const valid = Validators.email(value);
+        setEmailValidation(valid);
+        return valid;
+      }
+      case 'name': {
+        const valid = Validators.name(value);
+        setNameValidation(valid);
+        return valid;
+      }
+      case 'password': {
+        const valid = Validators.password(value);
+        setPasswordValidation(valid);
+        return valid;
+      }
+      default:
+        return true;
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let valid = true;
+    let formParams = {};
+
+    params[type].fields.forEach(field => {
+      if (!validateField(field.name, values[field.name].value)) {
+        valid = false;
+      }
+      formParams[field.name] = values[field.name].value;
+    });
+
+    if (!valid) {
+      return;
+    }
+
+    onAuth(formParams);
+  };
+
+  const values = {
+    email: {
+      value: email,
+      valid: emailValidation
+    },
+    password: {
+      value: password,
+      valid: passwordValidation
+    },
+    name: {
+      value: name,
+      valid: nameValidation
+    }
+  };
 
   return (
-    <>
+    <div className={ styles.Wrapper }>
       <Head>
-        <title>XTickets - { type.charAt(0).toUpperCase() + type.substring(1) }</title>
+        <title>XTickets - { params[type].title }</title>
       </Head>
-      <form onSubmit={ e => { e.preventDefault(); onSignin() } }>
-        <h1>{ type }</h1>
-        <div className="form-group">
-          <label>Email:</label>
-          <input value={ email } onChange={ e => setEmail(e.target.value) } className="form-control" type="email" />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input value={ password } onChange={ e => setPassword(e.target.value) } className="form-control" type="password" />
-        </div>
+      <Form onSubmit={ onSubmit }>
+        <h1>{ params[type].title }</h1>
+        { params[type].fields.map(field => (
+          <FloatingLabelInput
+            key={ field.id }
+            onChange={ onChange }
+            onBlur={ onBlur }
+            valid={ values[field.name].valid }
+            value={ values[field.name].value }
+            { ...field } />
+        )) }
         { errors }
-        <button className="btn btn-primary" type="submit">{ submitText }</button>
-      </form>
-    </>
+        <Button className={ styles.SubmitButton } size="lg" color="primary" type="submit">{ params[type].submitText }</Button>
+      </Form>
+    </div>
   );
 };
 
