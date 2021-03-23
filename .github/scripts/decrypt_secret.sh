ddcd8c4b9ce02b93b8bbdd8f6074f9ea6b8ac174
Guy Berkovich <mailofguy@gmail.com>
2021-03-23 10:09:25 +0200

    change path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
+mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_JSON_KEY" \
---output $HOME/xt-tickets/src/utils/xtickets-ab876349e8b0.json xtickets-ab876349e8b0.json.gpg
+--output $HOME/secrets/xtickets-ab876349e8b0.json xtickets-ab876349e8b0.json.gpg
