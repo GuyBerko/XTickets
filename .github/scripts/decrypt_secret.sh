8e7e1641356cefa4298c7fa87e3e352dea67a7d7
Guy Berkovich <mailofguy@gmail.com>
2021-03-23 09:51:09 +0200

    rm redundant

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_JSON_KEY" \
