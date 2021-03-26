df27d2af203e4d9b9bc8f43152415ba7a6cb97ad
Guy Berkovich <mailofguy@gmail.com>
2021-03-26 14:46:13 +0300

    test

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions 
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_JSON_KEY" \
---output ../../xt-tickets/xtickets-ab876349e8b0.json xtickets-ab876349e8b0.json.gpg
+--output xt-tickets/xtickets-ab876349e8b0.json xtickets-ab876349e8b0.json.gpg
