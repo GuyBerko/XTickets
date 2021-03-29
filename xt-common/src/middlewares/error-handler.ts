9ba6076d7d315477ff521b963362e2ea01b8eca2
Guy berkovich <mailofguy@gmail.com>
2021-03-29 16:33:48 +0300

    Update error-handler.ts
    rm log

--- xt-common/src/middlewares/error-handler.ts
+++ xt-common/src/middlewares/error-handler.ts
@@ -11,7 +11,6 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.error(err);
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }],
   });
