a5ece0108591653744a4ce3232e86554749da68f
Guy berkovich <mailofguy@gmail.com>
2021-03-29 17:34:28 +0300

    Update signout.test.ts
    fix typo

--- xt-auth/src/routes/__test__/signout.test.ts
+++ xt-auth/src/routes/__test__/signout.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 describe('SignOut Route', () => {
-  it('should clear the cookie after signing out', async () => {
+  it('should clear the session after signing out', async () => {
     await request(app)
       .post('/api/users/signup')
       .send({
