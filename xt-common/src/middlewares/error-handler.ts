5b0aff0169698dc89668711d4a17f1314a5e9849
Guy berkovich <mailofguy@gmail.com>
2021-03-29 16:38:01 +0300

    Update error-handler.ts
    fix lint

--- xt-common/src/middlewares/error-handler.ts
+++ xt-common/src/middlewares/error-handler.ts
@@ -8,7 +8,9 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    return res.status(err.statusCode).send({ 
+      errors: err.serializeErrors() 
+    });
   }
 
   res.status(400).send({
