2d380e4496275cf71af764cf4bfe32313dcdd594
Guy berkovich <mailofguy@gmail.com>
2021-03-29 17:27:27 +0300

    Update signup.ts
    add spaces

--- xt-auth/src/routes/signup.ts
+++ xt-auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+
 import { BadRequestError, validateRequest } from '@gb-xtickets/common';
+
 import { User } from '../models/user';
 import { UserCreatedPublisher } from '../events/publishers';
 import { natsClient } from '../nats-client';
